bd6d8ad783d3799b1fcac22312aa916c3e079098
Himanshu yadav <yadav.himanshu351@gmail.com>
2023-03-28 23:52:58 +0530

    channelInfo

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -6,3 +6,7 @@ export const YOUTUBE_VIDEO_API =
 
 export const YOUTUBE_SEARCH_API =
   "http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+
+export const YOUTUBE_CHANNEL_API =
+  "https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=UC_x5XG1OV2P6uZZ5FSM9Ttw&key=" +
+  GOOGLE_API_KEY;
